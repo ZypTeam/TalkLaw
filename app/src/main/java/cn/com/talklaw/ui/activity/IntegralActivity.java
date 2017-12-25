44b5b3b4def82f04eea854b4ce5d469e33835097
liangjie <liangjie@witmob>
2017-12-25 15:38:14 +0800

    添加积分首页

---
+++ app/src/main/java/cn/com/talklaw/ui/activity/IntegralActivity.java
@@ -0,0 +1,78 @@
+package cn.com.talklaw.ui.activity;
+
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.com.talklaw.R;
+import cn.com.talklaw.base.BaseTalkLawActivity;
+import cn.com.talklaw.ui.util.GlideImageLoader;
+import cn.com.talklaw.ui.view.HomeListProductView;
+
+/**
+ * @author zhaoyapeng
+ * @version create time:17/12/2515:31
+ * @Email zyp@jusfoun.com
+ * @Description ${积分首页}
+ */
+public class IntegralActivity extends BaseTalkLawActivity {
+    protected Banner banner;
+    protected HomeListProductView viewHotProduct;
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_homt_integral;
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+
+    @Override
+    public void initView() {
+        banner = (Banner) findViewById(R.id.banner);
+        viewHotProduct = (HomeListProductView) findViewById(R.id.view_hot_product);
+
+    }
+
+    @Override
+    public void initAction() {
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+        list.add("1");
+
+        banner.setImages(list);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.Default);
+        //设置标题集合（当banner样式有显示title时）
+//        banner.setBannerTitles(titles);
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(3000);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+
+
+        banner.setFocusable(true);
+        banner.setFocusableInTouchMode(true);
+        banner.requestFocus();
+
+        viewHotProduct.setData();
+    }
+}
--- app/src/main/java/cn/com/talklaw/ui/view/HomeTabIconView.java
+++ app/src/main/java/cn/com/talklaw/ui/view/HomeTabIconView.java
@@ -1,11 +1,15 @@
 package cn.com.talklaw.ui.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import cn.com.talklaw.R;
 import cn.com.talklaw.base.BaseView;
+import cn.com.talklaw.ui.activity.IntegralActivity;
 
 /**
  * @author zhaoyapeng
@@ -14,6 +18,8 @@ import cn.com.talklaw.base.BaseView;
  * @Description ${首页 专区 顾问 积分 tab 栏}
  */
 public class HomeTabIconView extends BaseView {
+    protected RelativeLayout layoutIntegral;
+
     public HomeTabIconView(Context context) {
         super(context);
     }
@@ -33,11 +39,18 @@ public class HomeTabIconView extends BaseView {
 
     @Override
     protected void initViews() {
-        LayoutInflater.from(mContext).inflate(R.layout.view_tab_icon_home,this,true);
+        LayoutInflater.from(mContext).inflate(R.layout.view_tab_icon_home, this, true);
+        layoutIntegral = (RelativeLayout) findViewById(R.id.layout_integral);
     }
 
     @Override
     protected void initActions() {
-
+        layoutIntegral.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, IntegralActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
