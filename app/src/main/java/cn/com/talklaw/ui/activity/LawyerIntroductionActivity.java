f0df59c4cee14cf088d66faeba62c1742dd47f31
wangcc <1123440793@qq.com>
2018-01-26 23:24:01 +0800

    新页面提交

---
+++ app/src/main/java/cn/com/talklaw/ui/activity/LawyerIntroductionActivity.java
@@ -0,0 +1,32 @@
+package cn.com.talklaw.ui.activity;
+
+import cn.com.talklaw.R;
+import cn.com.talklaw.base.BaseTalkLawActivity;
+
+/**
+ * @author wangcc
+ * @date 2018/1/26
+ * @describe 律师介绍
+ */
+
+public class LawyerIntroductionActivity extends BaseTalkLawActivity {
+    @Override
+    public int getLayoutResId() {
+        return R.layout.activity_lawyer_introduction;
+    }
+
+    @Override
+    public void initDatas() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initAction() {
+
+    }
+}
