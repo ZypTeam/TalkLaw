e4a63bb082edc8b9dcd27135b3c4a174c7a36453
lee <liliang_3991@163.com>
2018-01-29 23:01:57 +0800

    提交代码

--- app/src/main/java/cn/com/talklaw/ui/activity/AtaxCalculatorActivity.java
+++ app/src/main/java/cn/com/talklaw/ui/activity/AtaxCalculatorActivity.java
@@ -1,9 +1,13 @@
 package cn.com.talklaw.ui.activity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.jusfoun.baselibrary.Util.AppUtil;
+import com.jusfoun.baselibrary.Util.KeyBoardUtil;
 
 import cn.com.talklaw.R;
 import cn.com.talklaw.base.BaseTalkLawActivity;
@@ -20,13 +24,17 @@ import cn.com.talklaw.ui.widget.BackTitleView;
  */
 public class AtaxCalculatorActivity extends BaseTalkLawActivity {
     protected BackTitleView titleBar;
-    private int mTotalIncome = 0;
     private RelativeLayout rl_income_type;
 
-
+    private TextView tv_income_type_text,text_result;
     private CaseTypeDialog caseTypeDialog;
+    private EditText ed_total_income,ed_social_security;
+    private Button btn_reset,btn_submit;
 
     private String mIncomeType = "";
+
+    private int mTotalIncome = 0, mSocialSecurity = 0;
+    private int incomeTax = 0;
 
     @Override
     public int getLayoutResId() {
@@ -43,6 +51,14 @@ public class AtaxCalculatorActivity extends BaseTalkLawActivity {
     public void initView() {
         titleBar = (BackTitleView) findViewById(R.id.back_title_view);
         rl_income_type = findViewById(R.id.rl_income_type);
+        tv_income_type_text = findViewById(R.id.tv_income_type_text);
+
+        ed_total_income = findViewById(R.id.ed_total_income);
+        ed_social_security = findViewById(R.id.ed_social_security);
+        text_result = findViewById(R.id.text_result);
+
+        btn_reset = findViewById(R.id.btn_reset);
+        btn_submit = findViewById(R.id.btn_submit);
     }
 
     @Override
@@ -62,7 +78,81 @@ public class AtaxCalculatorActivity extends BaseTalkLawActivity {
             @Override
             public void getCaseType(CaseTypeModel.CaseTypeItemModel model) {
                 mIncomeType = model.title;
+                tv_income_type_text.setText(mIncomeType);
             }
         });
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetView();
+                KeyBoardUtil.hideSoftKeyboard(AtaxCalculatorActivity.this);
+            }
+        });
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+                KeyBoardUtil.hideSoftKeyboard(AtaxCalculatorActivity.this);
+            }
+        });
+
+    }
+
+    /**
+     * 重置
+     */
+    private void resetView(){
+        mTotalIncome = 0;
+        mSocialSecurity = 0;
+        ed_total_income.setText("");
+        ed_social_security.setText("");
+        tv_income_type_text.setText("");
+        mIncomeType = "";
+    }
+
+    /**
+     * 计算
+     */
+    private void calculate(){
+        try {
+            mTotalIncome = Integer.parseInt(ed_total_income.getText().toString());
+            mSocialSecurity = Integer.parseInt(ed_social_security.getText().toString());
+        }catch (Exception e){
+            mTotalIncome = 0;
+            mSocialSecurity = 0;
+        }
+
+        if(mTotalIncome == 0){
+            showToast("请输入收入金额");
+            return;
+        }
+
+        if(mSocialSecurity == 0){
+            showToast("请输入社保公积金");
+            return;
+        }
+        int personAmountOfIncome = mTotalIncome - mSocialSecurity - 3500;
+
+        if(personAmountOfIncome <= 0){
+            incomeTax = 0;
+        }else if(personAmountOfIncome <= 1500 && personAmountOfIncome > 0){
+            incomeTax = (int) (personAmountOfIncome * 0.03);
+        }else if(personAmountOfIncome <= 4500 && personAmountOfIncome > 1500){
+            incomeTax = (int) (personAmountOfIncome * 0.1) - 105;
+        }else if(personAmountOfIncome <= 9000 && personAmountOfIncome > 4500){
+            incomeTax = (int) (personAmountOfIncome * 0.2) - 555;
+        }else if(personAmountOfIncome <= 35000 && personAmountOfIncome > 9000){
+            incomeTax = (int) (personAmountOfIncome * 0.25) - 1005;
+        }else if(personAmountOfIncome <= 55000 && personAmountOfIncome > 35000){
+            incomeTax = (int) (personAmountOfIncome * 0.3) - 2755;
+        }else if(personAmountOfIncome <= 80000 && personAmountOfIncome > 55000){
+            incomeTax = (int) (personAmountOfIncome * 0.35) - 5505;
+        }else if(personAmountOfIncome > 80000){
+            incomeTax = (int) (personAmountOfIncome * 0.45) - 13505;
+        }
+
+        text_result.setText("个人所得税：" + incomeTax + "（元）");
     }
 }
